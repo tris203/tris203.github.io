49d231475660eaef90c700febf6ceb0bedbd6a32
tris203 <admin@snappeh.com>
2025-02-22 21:48:00 +0000

    feat: tailwind v4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ReactMarkdown from './components/ReactMarkdown';
 export default function HomePage() {
   const posts = getAllPosts();
   return (
-    <div className='pt=12 flex h-full flex-col bg-gradient-to-tr from-black to-gray-700 pt-24 md:h-screen'>
+    <div className='pt=12 flex h-full flex-col bg-linear-to-tr from-black to-gray-700 pt-24 md:h-screen'>
       <div className='container mx-auto px-4'>
         <div className='w-max'>
           <h2 className='overflow-hidden whitespace-nowrap  pr-5 text-5xl font-bold text-white'>
@@ -48,7 +48,7 @@ export default function HomePage() {
               <Link href='/blog'>
                 <button
                   type='button'
-                  className='rounded bg-gray-700 px-4 py-2 font-bold text-gray-100 hover:bg-gray-600'
+                  className='rounded-sm bg-gray-700 px-4 py-2 font-bold text-gray-100 hover:bg-gray-600'
                 >
                   View All Posts
                 </button>
